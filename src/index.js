526a2194505c9e55424d8918a62a1fc6c08a70c6
oksanabolbat <oksana.bolbat@gmail.com>
2021-03-07 13:04:28 +0200

    init: function created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let arr = [];
+    if (typeof matrix != "object" || matrix.length === 0) return arr;
+    arr = matrix.map(function (x, i) {
+        return i % 2 === 1 ? x.reverse() : x;
+    });
+    console.log(arr, " test");
+    return arr.flat();
+};
